b734630e7f462dce7480b7356ae312055c594479
gyevnarb <gyevnarb@gmail.com>
2020-01-19 01:13:16 +0000

    Runned R experiments

--- hugm/Program.cs
+++ hugm/Program.cs
@@ -4,7 +4,7 @@ using hugm.map;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace hugm
@@ -13,16 +13,12 @@ namespace hugm
     {
         static void Main(string[] args)
         {
-            /*
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
             Graph g = AreaUtils.Load("map.bin");
-            foreach (var group in g.V.GroupBy(n => (n as AreaNode).Areas[0].ElectoralDistrict))
-            {
-                Graph m = new Graph(group.ToList());
-                var c = m.GetConnectedComponents(true);
-                Console.WriteLine(c.Count);
-            }
-            */
-            MapToData();
+            //DisplayElectoralConnectedComponents(g);
+            MapToData(g);
+            //WriteNewPartitionGraph(g);
             Console.ReadLine();
         }
 
@@ -41,7 +37,7 @@ namespace hugm
             Console.WriteLine("Merged successfully");
         }
 
-        private static void MapToData()
+        private static void MapToData(Graph m)
         {
             Console.Write("Path to map: ");
             string map = Console.ReadLine();
@@ -49,9 +45,7 @@ namespace hugm
 
             Console.Write("Write path: ");
             string output = Console.ReadLine();
-            if (output == "") output = "./";
-
-            Graph m = AreaUtils.Load(map);
+            if (output == "") output = "../../";
 
             using (StreamWriter sw = new StreamWriter(output + @"maplist.csv"))
                 foreach (AreaNode node in m.V)
@@ -78,6 +72,53 @@ namespace hugm
                 }
             }
             Console.WriteLine($"Electoral districts results written to {output + @"votes.csv"}");
+
+            using (StreamWriter sw = new StreamWriter(output + "ssdist.csv"))         
+            {
+                for (int i = 0; i < m.V.Count; i++)
+                {
+                    double dist = GetDistance(m.V[i], m.V[0]);
+                    sw.Write($"{dist:F2}");
+
+                    for (int j = 1; j < m.V.Count; j++)
+                    {
+                        dist = GetDistance(m.V[i], m.V[j]);
+                        sw.Write($";{dist:F2}");
+                    }
+                    sw.Write("\n");
+                }
+            }
+        }
+
+        private static void DisplayElectoralConnectedComponents(Graph g)
+        {
+            foreach (var group in g.V.GroupBy(n => (n as AreaNode).Areas[0].ElectoralDistrict))
+            {
+                Graph m = new Graph(group.ToList());
+                var c = m.GetConnectedComponents(true);
+                Console.WriteLine($"{group.Key}: {c.Count}");
+            }
+        }
+
+        private static void WriteNewPartitionGraph(Graph g)
+        {
+            Console.WriteLine("Writing new assignment");
+            Random r = new Random();
+            var file = File.ReadAllLines(@"..\..\..\visualizer\data\partitions.csv").Select(x => x.Split(',')).Select(x => x.Select(y => int.Parse(y) + 1).ToList()).ToList();
+            var assignment = file[r.Next(file.Count)];
+            int i = 0;
+            foreach (AreaNode vert in g.V)
+            {
+                vert.Areas[0].ElectoralDistrict = assignment[i];
+                i++;
+            }
+            AreaUtils.Save("../../map_new.bin", g);
+            Console.WriteLine("Created new electoral assignment under file map_new.bin");
+        }
+
+        private static double GetDistance(Node n1, Node n2)
+        {
+            return Math.Pow(n1.X - n2.X, 2) + Math.Pow(n1.Y - n2.Y, 2);
         }
     }
 }
--- hugm/markov_analysis.R
+++ hugm/markov_analysis.R
@@ -1,3 +1,5 @@
+library(redist)
+
 create_dataset <- function(path = "budapest.RData") {
   ## Load adjacency list
   x <- scan("maplist.csv", what="", sep="\n")
@@ -13,31 +15,81 @@ create_dataset <- function(path = "budapest.RData") {
   ## Load election results
   results <- read.table("votes.csv", sep=",", header=TRUE)
 
+  # Load approximate square distances
+  ssdist <- as.matrix(read.csv("ssdist.csv", sep=";", header=FALSE))
+
   ## Save dastaset
-  bud <- list(adjobj=adjobj, popvec=popvec, inicds=initcds, results=results)
+  bud <- list(adjobj=adjobj, popvec=popvec, initcds=initcds, results=results, ssdist=ssdist)
   save(bud, file=path)
-  bud
 }
 
-run_analysis <- function(nsims=10000, ndists=18, popcons=0.15, seed=1) {
-  d <- load("budapest.RData")
-  set.seed(seed)
+find_params <- function(nsims=10000, ndists=18) {
+  load("budapest.RData")
+  params <- expand.grid(eprob = c(.01, .05, .1), lambda = c(0, 1, 2, 5, 10), beta = c(0, 5, 10, 50, 250, 750, 2500))
 
-  params <- expand.grid(eprob=c(.01, .05, .1))
   p <- redist.findparams(adjobj=bud$adjobj,
     popvec=bud$popvec,
     nsims=nsims, ndists=ndists,
     initcds=bud$initcds,
     params=params
   )
+}
+
+run_simulation <- function(nsims=10000, ndists=18, popcons=0.15, seed=1, nloop=1, savename="bud") {
+  load("budapest.RData")
+  set.seed(seed)
 
   bud_alg <- redist.mcmc(adjobj=bud$adjobj,
     popvec=bud$popvec,
-    nsims=bud$nsims, ndists=bud$ndists,
+    nsims=nsims, ndists=ndists,
     initcds=bud$initcds,
-    popcons=bud$popcons)
+    popcons=popcons,
+    ssdmat=bud$ssdist,
+    beta=9,
+    constraint="compact",
+    nloop=nloop,
+    savename=savename
+  )
 
-  bud_dmi <- redist.segcalc(bud_alg, bud$results$FideszKDNP, bud$popvec)
+  write.csv(t(bud_alg$partitions), "partitions.csv", row.names = FALSE)
+  #save(bud_alg, file="bud_alg.RData")
+
+  if (nloop > 1) {
+    bud_out <- redist.combine(
+      savename=savename,
+      nsims=nsims,
+      nloop=nloop,
+      nthin=10,
+      nunits=length(bud$adjobj)
+    )
+  }
+
+  #save(bud_out, file="bud_out.RData")
 }
 
-create_dataset()
+run_analysis <- function(savename="bud", nsims=10000, nloop=10) {
+  load("budapest.RData")
+
+  bud_out <- redist.combine(
+    savename=savename,
+    nsims=nsims,
+    nloop=nloop,
+    nthin=10,
+    nunits=length(bud$adjobj)
+  )
+
+  bud_dmi <- redist.segcalc(bud_out, bud$results$FideszKDNP, bud$popvec)
+
+  plot.new()
+  lines(density(bud_dmi, from = 0, to = 1), col = "red", lty = 5)
+
+  #redist.diagplot(bud_dmi, plot = "trace")
+  #redist.diagplot(bud_dmi, plot = "autocorr")
+  #redist.diagplot(bud_dmi, plot = "densplot")
+  #redist.diagplot(bud_dmi, plot = "mean")
+}
+
+# create_dataset()
+run_simulation()
+# run_analysis()
+# find_params()
